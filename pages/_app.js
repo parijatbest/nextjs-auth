d19865a3c1fe0fd13ee585a7a1543e15ef833127
Parijat Mandal <parijat.it@gmail.com>
2024-06-04 21:48:00 +0530

    update session

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,9 @@ import { SessionProvider } from "next-auth/react";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={session}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
